b5909fc3c7f82682f7f5c25d2a95a6673d7b4be4
Dmitry Lazurkin <dilaz03@gmail.com>
2016-11-22 23:56:41 +0300

    kafka: fake ControlledShutdowd api key

--- controlled_shutdown.py
+++ controlled_shutdown.py
@@ -1,8 +1,8 @@
 
 from kafka.protocol.struct import Struct
-from kafka.protocol.types import Int32, Schema
+from kafka.protocol.types import Int32, Schema, String, Int16, Array
 
-from util import send
+from util import send, send_with_response
 
 API_KEY = 7
 
@@ -14,8 +14,28 @@ class ControlledShutdownRequest_v0(Struct):
         ('broker_id', Int32)
     )
 
-send(
+
+class ControlledShutdownResponse_v0(Struct):
+    API_KEY = API_KEY
+    API_VERSION = 0
+    SCHEMA = Schema(
+        ('error_code', Int16),
+        ('partitions_remaining', Array(
+            ('topic', String('utf-8')),
+            ('partition', Int32),
+        ))
+    )
+
+
+send_with_response(
     ControlledShutdownRequest_v0(
         broker_id=1000,
+    ),
+    ControlledShutdownResponse_v0(
+        error_code=0,
+        partitions_remaining=[
+            ('topic1', 0),
+            ('topic2', 1)
+        ]
     )
 )
--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 
 import socket
+import threading
 
 from io import BytesIO
 
@@ -7,6 +8,7 @@ from kafka.protocol.api import RequestHeader
 from kafka.protocol.types import Int32
 
 
+PORT = 9092
 correlation_id = 0
 
 
@@ -14,7 +16,7 @@ def send(request):
     global correlation_id
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('127.0.0.1', 9092))
+    s.connect(('127.0.0.1', PORT))
 
     header = RequestHeader(
         request=request,
@@ -33,3 +35,28 @@ def send(request):
     s.recv(size, socket.MSG_WAITALL)
 
     s.close()
+
+
+def send_with_response(request, response):
+    event = threading.Event()
+
+    def server_thread():
+        ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ss.bind(('127.0.0.1', PORT))
+        ss.listen(1)
+        event.set()
+        (cs, _) = ss.accept()
+
+        size = Int32.decode(BytesIO(cs.recv(4, socket.MSG_WAITALL)))
+        id = Int32.decode(BytesIO(cs.recv(4, socket.MSG_WAITALL)))
+        cs.recv(size - 4, socket.MSG_WAITALL)
+
+        message = b''.join([Int32.encode(id), response.encode()])
+        cs.send(Int32.encode(len(message)) + message)
+
+        cs.close()
+        ss.close()
+
+    threading.Thread(target=server_thread).start()
+    event.wait()
+    send(request)
