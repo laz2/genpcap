2b04c6a9ace4d406ebd85e90377bf466a35fcde7
Dmitry Lazurkin <dilaz03@gmail.com>
2016-11-23 01:30:28 +0300

    kafka: close socket by with statement

--- util.py
+++ util.py
@@ -16,46 +16,43 @@ def send(request):
     global correlation_id
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(('127.0.0.1', PORT))
+    with s:
+        s.connect(('127.0.0.1', PORT))
 
-    header = RequestHeader(
-        request=request,
-        correlation_id=correlation_id,
-        client_id='test'
-    )
-    correlation_id += 1
+        header = RequestHeader(
+            request=request,
+            correlation_id=correlation_id,
+            client_id='test'
+        )
+        correlation_id += 1
 
-    message = b''.join([header.encode(), request.encode()])
-    size = Int32.encode(len(message))
-    s.send(size)
-    s.send(message)
+        message = b''.join([header.encode(), request.encode()])
+        size = Int32.encode(len(message))
+        s.send(size)
+        s.send(message)
 
-    size = s.recv(4, socket.MSG_WAITALL)
-    size = Int32.decode(BytesIO(size))
-    s.recv(size, socket.MSG_WAITALL)
-
-    s.close()
+        size = s.recv(4, socket.MSG_WAITALL)
+        size = Int32.decode(BytesIO(size))
+        s.recv(size, socket.MSG_WAITALL)
 
 
 def send_with_response(request, response):
     event = threading.Event()
 
     def server_thread():
-        ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        ss.bind(('127.0.0.1', PORT))
-        ss.listen(1)
-        event.set()
-        (cs, _) = ss.accept()
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as ss:
+            ss.bind(('127.0.0.1', PORT))
+            ss.listen(1)
+            event.set()
 
-        size = Int32.decode(BytesIO(cs.recv(4, socket.MSG_WAITALL)))
-        id = Int32.decode(BytesIO(cs.recv(4, socket.MSG_WAITALL)))
-        cs.recv(size - 4, socket.MSG_WAITALL)
+            (cs, _) = ss.accept()
+            with cs:
+                size = Int32.decode(BytesIO(cs.recv(4, socket.MSG_WAITALL)))
+                id = Int32.decode(BytesIO(cs.recv(4, socket.MSG_WAITALL)))
+                cs.recv(size - 4, socket.MSG_WAITALL)
 
-        message = b''.join([Int32.encode(id), response.encode()])
-        cs.send(Int32.encode(len(message)) + message)
-
-        cs.close()
-        ss.close()
+                message = b''.join([Int32.encode(id), response.encode()])
+                cs.send(Int32.encode(len(message)) + message)
 
     threading.Thread(target=server_thread).start()
     event.wait()
